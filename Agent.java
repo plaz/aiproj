403d8a04bd8ec177d6950595d6511f9b3364b4cc
David Plassmann <dp4487a@american.edu>
2009-12-11 14:13:48 -0500

    Second meeting

--- Agent.java
+++ Agent.java
@@ -44,6 +44,8 @@ public class Agent{
   static int state;
   static int cycles;
   static boolean haveIdle;
+  static int scoutDir=0;
+  static int lastScoutYear=Integer.MAX_VALUE;
   
   
   static int lastHouseStarted=Integer.MAX_VALUE;
@@ -56,7 +58,11 @@ public class Agent{
       if (cycles==0){
 	System.out.println("In all prompts, when requested to pause, press 'F3'.");
 	System.out.println("Also note that to select the village center, you can press 'h' instead of clicking.");
-	System.out.println("Click the select idle worker button, then pause.");
+	System.out.println();
+	System.out.println("First, pause the game.");
+	System.out.println("Click the menu button in the top-right corner of the screen, select options,");
+	System.out.println("and change the resolution to 1280x1024.");
+	System.out.println("Click the select idle worker button.");
       } else {
 	getState();
 	System.out.println();
@@ -67,7 +73,7 @@ public class Agent{
       System.out.println();
       dump();
 
-      
+      int distance = 1;
       
       for (int i=0; i<5; i++){
 	if (decideOnScreenAction()){
@@ -75,11 +81,17 @@ public class Agent{
 	  break;
 	} else {
 	  if (i==4){
-	    decideFallBack();
+	    if (desperate()){
+	      distance++;
+	      i=-1;
+	      continue;
+	    } else {
+	      decideFallBack();
+	    }
 	    instruct();
 	    break;
 	  }
-	  System.out.println("Move your view to the frame one screen "+directions[i]+" of the village center");
+	  System.out.println("Move your view to the frame "+distance+" screen "+directions[i]+" of the village center");
 	}
       }
       
@@ -88,14 +100,17 @@ public class Agent{
 
   public static void instruct(){
     System.out.println();
+    scout();
+    System.out.println();
+    System.out.println("Unpause the game. Then,");
     if (!haveIdle){
-      System.out.println("If a message appears on-screen or years left are "+(years-10)+" or less,");
+      System.out.println("if a message appears on-screen or years left are "+(years-10)+" or less,");
       System.out.println("then click on any open ground, click the idle worker button, pause, and report back.");
       state = WAITING;
     }
     else{
-      System.out.println("Immediately after performing the above action,");
-      System.out.println("click on any open ground, click the idle worker button, pause, and report back.");
+      System.out.println("immediately after performing the above action,");
+      System.out.println("pause, click on any open ground, click the idle worker button, and report back.");
       state = LOOPBACK;
     }
     System.out.println();
@@ -141,7 +156,23 @@ public class Agent{
   }
 
   public static void decideFallBack(){
-    //TODO Fill method
+    haveIdle = false;
+    if (state == HOUSE_BUILT
+	|| state == WORKER_BUILT
+	|| state == FARM_DEAD)
+      haveIdle=true;
+    else {
+      System.out.print("Is a villager selected? ");
+      haveIdle = getYesNo();
+    }
+
+    if (haveIdle && onScreen("gold")){
+      mineGold();
+    } else if (haveIdle && onScreen("rock")) {
+      mineRock();
+    } else if (haveIdle && onScreen("fish")) {
+      fish();
+    }
   }
 
   public static boolean decideOnScreenAction(){
@@ -156,24 +187,28 @@ public class Agent{
     }
 
 
+    System.out.println();
     if(canBuildHouse()){
       buildHouse();
     } else if(canBuildWorker()){
       buildWorker();
-    } else if(haveIdle && onScreen("deer")){
-      hunt();
-    } else if(haveIdle && onScreen("sheep")){
-      herd();
-    } else if(haveIdle && onScreen("bushes")){
-      forage();
-    } else if(haveIdle && farmIsPriority() && canBuildFarm()){
-      farm();
-    } else if(haveIdle && onScreen("trees")){
-      chop();
-    } else if(haveIdle && canBuildFarm()){
-      farm();
-    } else{
-      return false;
+    } else if (haveIdle){ 
+      System.out.println("\nClick the select idle worker button.\n");
+      if(onScreen("deer")){
+	hunt();
+      } else if(onScreen("sheep")){
+	herd();
+      } else if(onScreen("bushes")){
+	forage();
+      } else if(farmIsPriority() && canBuildFarm()){
+	farm();
+      } else if(onScreen("trees")){
+	chop();
+      } else if(canBuildFarm()){
+	farm();
+      } else{
+	return false;
+      }
     }
 
     return true;
@@ -185,33 +220,76 @@ public class Agent{
 	&& popLimit<MAX_POP 
 	&& years>MIN_HOUSE_YEARS
 	&& resources[WOOD]>=HOUSE_COST
-	&& resources[FOOD]>=MIN_HOUSE_FOOD){
+	&& resources[FOOD]>=MIN_HOUSE_FOOD)
+    {
       System.out.println("Search revealed area. Is there a partially completed house being worked on? ");
       return !getYesNo();
     }
+    else return false;
   }
 
   public static void buildHouse(){
-    System.out.println("BUILD HOUSE");
-    //FIXME fill method
+    if(!haveIdle){
+      System.out.println("\nUnpause the game. Wait until any worker approaches the town center.");
+      System.out.println("After the worker turns away from the town center, click it to select and repause.");
+      System.out.println("Then carry out the following task:\n");
+    }
+    System.out.println("Click the buildings button, click the house button.");
+    System.out.println("Move your view frame to center on the town center, and move the outline of the house");
+    System.out.println("to the top left corner of the screen. If it is not red, click to build a house.");
+    System.out.println("If it is red, move the outline clockwise around the screen until it turns white, then click.");
+    System.out.println("If you return to the top left corner, make the same loop one house-width out.");
+    System.out.println("Repeat until success or you leave the revealed area.");
+  }
+
+  public static boolean desperate(){
+    //Worker
+    if (pop<popLimit
+	&& resources[FOOD]<WORKER_COST
+	&& years >= MIN_WORKER_YEARS+100)
+      return true;
+    
+    //House
+    if (popLimit<MAX_POP
+	&& resources[WOOD]<HOUSE_COST
+	&& years >= MIN_HOUSE_YEARS+100)
+      return true;
+
+    return false;
   }
 
 
-  public static boolean canBuildWorker()
-  {
+  public static boolean canBuildWorker() {
     if (pop<popLimit
 	&& resources[FOOD]>=WORKER_COST
 	&& years >= MIN_WORKER_YEARS)
       {
-	System.out.print("Unpause, select the town center, and repause. Is a villager being built? ");
+	System.out.print("Select the town center. Is a villager being built? ");
 	boolean villagerBuilt = getYesNo();  
 	if (villagerBuilt){
-	  System.out.println("Please unpause, click the select idle worker button, and repause.");
+	  System.out.println("Click the select idle worker button.");
 	}
 	return !villagerBuilt;
       }
     else
       return false;
+  }
+
+  public static void scout(){
+    if (lastScoutYear == Integer.MAX_VALUE)
+      {}
+    else if (years > (lastScoutYear - 5))
+      return;
+
+    System.out.println("Search the revealed area to find and click on the scout.");
+    System.out.println("On the minimap, put the point of your cursor on the white dot,");
+    System.out.println("Then move the cursor "+directions[scoutDir]+" until the point is parallel to ");
+    System.out.println("the "+directions[scoutDir]+"ernmost revealed land.");
+    System.out.println("If the tip of the cursor is now off the minimap, move in the reverse direction");
+    System.out.println("until it is back on the minimap. Right click to send the scout.");
+
+    scoutDir = (scoutDir+1)%4;
+    lastScoutYear = years;
   }
 
   public static void buildWorker(){
@@ -231,12 +309,29 @@ public class Agent{
     System.out.println("Right-click on the bush closest to the selected worker.");
   }
   public static void farm(){
-    System.out.println("BUILD FARM");
-    //FIXME fill method
+    System.out.println("Click the buildings button. Click the farm button.");
+    System.out.println("Move your view frame to the town center.");
+    System.out.println("Move the outline of the farm over the town center, then move it up just until it no longer is.");
+    System.out.println("If the outline is white, click to build the farm.");
+    System.out.println("Otherwise, move the outline clockwise around the town center until it becomes white.");
+    System.out.println("If you return to where you started, move the outline north and repeat until success.");
   }
   public static void chop(){
     System.out.println("Right-click on the tree closest to the selected worker.");
   }
+
+  public static void mineGold(){
+    System.out.println("Right-click on the gold pile closest to the selected worker.");
+  }
+
+  public static void mineRock(){
+    System.out.println("Right-click on the rock pile closest to the selected worker.");
+  }
+
+  public static void fish(){
+    System.out.println("Right-click on the fish closest to the selected worker.");
+  }
+
   public static boolean farmIsPriority(){
     return (resources[WOOD]>=FARM_WOOD_THRESH 
 	    && years > MIN_FARM_YEARS);
@@ -265,18 +360,24 @@ public class Agent{
 
   public static void getResources(){
     for (int i=0; i<4; i++){
-      System.out.print("Amount of stored "+names[i]+"? ");
-      resources[i]=Integer.parseInt(in.nextLine());      
+      resources[i]=getNum("Amount of stored "+names[i]+"? ");
     }
     
-    System.out.print("Current population? ");
-    pop = Integer.parseInt(in.nextLine());
-    
-    System.out.print("Current population limit? ");
-    popLimit = Integer.parseInt(in.nextLine());
+    pop=getNum("Current population? ");
+    popLimit=getNum("Current population limit? ");
+    years=getNum("Years remaining? ");
+  }
 
-    System.out.print("Years remaining? ");
-    years = Integer.parseInt(in.nextLine());
+  public static int getNum(String prompt){
+    while(true){
+      System.out.print(prompt);
+      String response = in.nextLine();
+      try{
+	return Integer.parseInt(response);
+      } catch (Exception e) {
+	System.out.println("Parsing error. Try again.");
+      }
+    }
   }
 
   public static void dump(){
@@ -285,7 +386,7 @@ public class Agent{
       System.err.print(names[i]+":"+resources[i]+" ");
     }
     System.err.println(pop+"/"+popLimit+" "+years);
-    System.err.println("Cycles:"+cycles+" Idle:"+haveIdle);
+    System.err.println("Cycles:"+cycles+" Idle:"+haveIdle+" Last scout:"+lastScoutYear);
     System.err.println("---------------------------------------");
   }
 }
